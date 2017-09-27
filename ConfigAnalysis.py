f2eb5e84946917f773ace0a11026baca51124c2c
li-yi-cloud <30892553+li-yi-cloud@users.noreply.github.com>
2017-09-27 15:35:57 +0800

    Create ConfigAnalysis.py

---
+++ ConfigAnalysis.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+import re
+
+class ConfigAnalysis():
+    def __init__(self):
+        self.__config = None
+    def __validitycheck(self,lines,configfilename):
+        for line_number in range(len(lines)):
+            if (not re.match(r'^#', lines[line_number])) and (not re.match(r'^(\r\n|\r|\n)',lines[line_number])) and (not re.match(r'^[a-zA-Z0-9](\w|\.| |\t)*\=( |\t)*(\S){1,}', lines[line_number])):
+                raise SyntaxError("The configuration file (%s) have SyntaxError(%s) in line %s"%(configfilename,lines[line_number],line_number))
+    def __analysisfile(self,configfilename):
+        with open(configfilename,'r') as Contents:
+            lines = Contents.readlines()
+        self.__validitycheck(lines,configfilename)
+        return [re.match(r'^[a-zA-Z0-9](\w|\.| |\t)*\=( |\t)*(\S){1,}', line).group() for line in lines if re.match(r'^[a-zA-Z0-9](\w|\.| |\t)*\=( |\t)*(\S){1,}', line)]
+    def __analysislines(self,vaild_lines):
+        return [re.split(r'( |\t)*\=( |\t)*',i,maxsplit=1) for i in vaild_lines]
+    def read(self,configfilename):
+        self.__config = [(item[0],item[-1]) for item in  self.__analysislines(self.__analysisfile(configfilename))]
+    def keys(self):
+        return [ckey for ckey,_ in self.__config]
+    def get(self,key):
+        for ckey,cvalue in self.__config:
+            if ckey == key:
+                return cvalue
+        raise KeyError("Maping key not found.('%s')"%key)
+
+if __name__=="__main__":
+    pass
