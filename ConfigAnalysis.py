678df0dd6f21443dd8a8908c96142878cae13bd2
li-yi-cloud <li-yi-cloud@users.noreply.github.com>
2017-10-20 09:33:53 +0800

    change config object

--- ConfigAnalysis.py
+++ ConfigAnalysis.py
@@ -6,7 +6,7 @@ class ConfigAnalysis():
     def __init__(self):
         self.__update_log = {}
         self.__add_log = {}
-        self.__config = None
+        self.__config = {}
         self.__configfilename = None
     def __validitycheck(self,lines,configfilename):
         for line_number in range(len(lines)):
@@ -19,34 +19,38 @@ class ConfigAnalysis():
         return [re.match(r'^[a-zA-Z0-9](\w|\.| |\t)*\=( |\t)*(\S){1,}', line).group() for line in lines if re.match(r'^[a-zA-Z0-9](\w|\.| |\t)*\=( |\t)*(\S){1,}', line)]
     def __analysislines(self,vaild_lines):
         return [re.split(r'( |\t)*\=( |\t)*',i,maxsplit=1) for i in vaild_lines]
+    def __genarate_config(self,key,value):
+        self.__config[key]=value
     def read(self,configfilename):
         if os.path.exists(configfilename):
             if os.path.isfile(configfilename):
-                self.__config = [(item[0],item[-1]) for item in  self.__analysislines(self.__analysisfile(configfilename))]
+                _ = [self.__genarate_config(item[0],item[-1]) for item in  self.__analysislines(self.__analysisfile(configfilename))]
                 self.__configfilename = configfilename
             else:
                 raise IOError('"%s" is not a file.'%configfilename)
         else:
             raise FileNotFoundError('No such file or directory:"%s"'%configfilename)
     def keys(self):
-        return [ckey for ckey,_ in self.__config]
+        return self.__config.keys()
     def get(self,key):
-        for ckey,cvalue in self.__config:
-            if ckey == key:
-                return cvalue
-        raise KeyError("Maping key not found.('%s')"%key)
+        try:
+            return self.__config[key]
+        except:
+            raise KeyError("Maping key not found.('%s')"%key)
     def update(self,key,newvalue):
         if re.match(r"(\S){1,}",newvalue) and re.match(r"^(\S){1,}",newvalue).group()==newvalue:
             self.__update(key,newvalue)
         else:
             raise ValueError("Unsupported value.(Only support Non-blank characters)")
     def __update(self,key,newvalue):
-        for i in range(len(self.__config)):
-            if self.__config[i][0]==key:
-                self.__config[i]=(key,newvalue)
+        if key in self.__config.keys():
+            if self.__config[key]!=newvalue:
+                self.__config[key]=newvalue
                 self.__update_log[key]=newvalue
-                return True
-        raise KeyError("Maping key not found.('%s')"%key)
+            else:
+                pass
+        else:
+            raise KeyError("Maping key not found.('%s')"%key)
     def add(self,key,value):
         if re.match(r"(\S){1,}",value) and re.match(r"^(\S){1,}",value).group()==value:
             if re.match(r'^[a-zA-Z0-9](\w|\.)*$',key):
@@ -56,11 +60,10 @@ class ConfigAnalysis():
         else:
             raise ValueError("Unsupported value.(Only support Non-blank characters)")
     def __add(self,key,value):
-        try:
-            self.update(key, value)
+        if key in self.__config.keys():
             self.__update(key, value)
-        except:
-            self.__config.append((key,value))
+        else:
+            self.__config[key]=value
             self.__add_log[key]=value
     def save(self):
         with open(self.__configfilename,"r") as cfile:
@@ -77,4 +80,3 @@ class ConfigAnalysis():
             
 if __name__=="__main__":
     pass
-
